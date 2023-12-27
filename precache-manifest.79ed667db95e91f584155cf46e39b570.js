2cb82e3959e7dd7a3755d0df042c06afb0f70307
Max Greenwald <powerslab3@gmail.com>
2023-12-27 15:55:03 -0500

    Updates

---
+++ precache-manifest.79ed667db95e91f584155cf46e39b570.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586fe1a9866afa7877dd5c738c9d4ce1",
+    "url": "/achshorttes1/index.html"
+  },
+  {
+    "revision": "3765ad00ca8872ddb4b9",
+    "url": "/achshorttes1/static/css/main.f4ac948c.chunk.css"
+  },
+  {
+    "revision": "5d915751b9f404a172c8",
+    "url": "/achshorttes1/static/js/2.cd9cfe3b.chunk.js"
+  },
+  {
+    "revision": "ee45c4ac1e2cdbe20661beb901b1137f",
+    "url": "/achshorttes1/static/js/2.cd9cfe3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3765ad00ca8872ddb4b9",
+    "url": "/achshorttes1/static/js/main.1b2f9562.chunk.js"
+  },
+  {
+    "revision": "53e8cfc74baadc0213d2",
+    "url": "/achshorttes1/static/js/runtime-main.ec243dc2.js"
+  },
+  {
+    "revision": "6956dbda0015b26b8daac462ec922ef5",
+    "url": "/achshorttes1/static/media/psych_logo.6956dbda.jpg"
+  },
+  {
+    "revision": "d9fbf22f557b87c921a73ef996721520",
+    "url": "/achshorttes1/static/media/rating_keydown_1.d9fbf22f.png"
+  },
+  {
+    "revision": "6a659dd41fa6c15739f611662bf6d8d1",
+    "url": "/achshorttes1/static/media/rating_keydown_2.6a659dd4.png"
+  },
+  {
+    "revision": "d955b91a2b4ba7dca40ee5ccb82d995b",
+    "url": "/achshorttes1/static/media/rating_keydown_3.d955b91a.png"
+  },
+  {
+    "revision": "c8b7c6860406752a1915e08a432cb320",
+    "url": "/achshorttes1/static/media/rating_keydown_4.c8b7c686.png"
+  },
+  {
+    "revision": "34958048ce563b1140c19e19f6e72410",
+    "url": "/achshorttes1/static/media/rating_keydown_5.34958048.png"
+  }
+]);
